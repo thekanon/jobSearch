4346dd3b043f9c20a6a6bff11ccfc17266064697
thekanon <dlengjs12@naver.com>
2023-05-04 13:25:05 +0900

    사용하지 않는 코드 삭제 및 autoComplete 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,12 @@
-import React, { useContext } from 'react';
-import styled from 'styled-components';
-import { ThemeContext } from '../contexts/ThemeContext';
-import AutoComplete from '@/components/molecules/AutoComplete';
-import {
-  JobType,
-  MajorType,
-  experienceLevels,
-} from '@/lib/categories'
-
+import React, { useContext } from "react";
+import styled from "styled-components";
+import { ThemeContext } from "../contexts/ThemeContext";
+import AutoComplete from "@/components/molecules/AutoComplete";
+import { JobType, MajorType, experienceLevels } from "@/lib/categories";
 
 const HomePage = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     console.log(e.target.value);
     setValue(e.target.value);
@@ -22,14 +17,10 @@ const HomePage = () => {
       <AutoComplete
         value={value}
         onChange={onChange}
-        placeholder={'직무, 연차를 입력해주세요'}
-        textArray={[
-          ...JobType,
-          ...MajorType,
-          ...experienceLevels,
-        ]}
+        placeholder={"직무, 연차를 입력해주세요"}
+        textArray={[...JobType, ...MajorType, ...experienceLevels]}
         styleProps={{
-          border: '1px solid gray',
+          border: "1px solid gray",
         }}
       />
     </MainContainer>
@@ -43,17 +34,6 @@ const MainContainer = styled.div`
   flex-direction: column;
   background-color: ${({ theme }) => theme.body};
   color: ${({ theme }) => theme.text};
-`;
-
-const ToggleButton = styled.button`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  background: ${({ theme }) => theme.toggleBorder};
-  color: ${({ theme }) => theme.text};
-  border: none;
-  padding: 10px;
-  cursor: pointer;
 `;
 
 export default HomePage;
