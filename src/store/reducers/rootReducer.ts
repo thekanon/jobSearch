4f4b12827e68b00bddca302f1fe415197075d08c
Doo.Lee <dlengjs123@gmail.com>
2023-07-09 19:14:08 +0900

    메인 페이지 구현

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -3,10 +3,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import counterReducer from "./counter";
 import categoriesReducer from "./categories";
+import selectChipReducer from "./selectChip";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   categories: categoriesReducer,
+  selectChip: selectChipReducer,
 });
 
 export default rootReducer;
