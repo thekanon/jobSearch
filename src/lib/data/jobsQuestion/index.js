9707729e867f27af4e945d02000be222695bf642
thekanon <dlengjs12@naver.com>
2023-11-06 20:50:57 +0900

    api 연결
     변경 사항에 대한 커밋 메시지를 입력하십시오. '#' 문자로 시작하는

---
+++ src/lib/data/jobsQuestion/index.js
@@ -0,0 +1,11 @@
+import backendDeveloper from "./backendDeveloper.json";
+import frontendDeveloper from "./frontendDeveloper.json";
+import performanceMarketers from "./performanceMarketers.json";
+import visualDesigner from "./visualDesigner.json";
+
+export default {
+  backendDeveloper,
+  frontendDeveloper,
+  performanceMarketers,
+  visualDesigner,
+};
--- src/pages/api/jobs/[jobId].ts
+++ src/pages/api/jobs/[jobId].ts
@@ -1,40 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import axios from "axios";
-import path from "path";
-import fs from "fs/promises";
+import jobsData from "@/lib/data/jobsQuestion";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const jobId = req.query.jobId as string;
+  const {
+    query: { jobId },
+  } = req;
 
-  console.log("test");
+  console.log("jobId", jobId);
 
-  // JSON 파일의 경로를 생성합니다.
-  const filePath = path.join(
-    __dirname,
-    "..",
-    "..",
-    "..",
-    "..",
-    "..",
-    "public",
-    "data",
-    "jobsQuestion",
-    `${jobId}.json`
-  );
+  const jobData = jobsData[jobId];
 
-  try {
-    // 파일을 읽습니다.
-    const fileContents = await fs.readFile(filePath, "utf8");
-
-    // JSON을 파싱합니다.
-    const jobData = JSON.parse(fileContents);
-
-    console.log(fileContents);
-
-    // 응답으로 JSON 데이터를 보냅니다.
-    res.status(200).json(jobData);
-  } catch (error) {
-    console.error(error);
-    res.status(404).json({ message: "Job not found" });
+  if (!jobData) {
+    return res.status(404).json({ message: "Job not found" });
   }
+
+  res.status(200).json(jobData);
 };
