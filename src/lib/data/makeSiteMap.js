4c76c934f958c911896e72b49d3e4a4c6e1541e7
thekanon <dlengjs12@naver.com>
2023-11-27 21:50:51 +0900

    사이트맵 수정

--- src/lib/data/makeSiteMap.js
+++ src/lib/data/makeSiteMap.js
@@ -9,8 +9,13 @@ fs.readdir(directoryPath, function (err, files) {
     return console.log("Unable to scan directory: " + err);
   }
 
-  let sitemap =
-    '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+    `;
 
   // Adding the home page
   sitemap +=
